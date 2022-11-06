55a7c402956b0f2682aa4ccc1b74ca8e65ecf0cd
nettanetta <nettanetta@users.noreply.github.com>
2022-11-06 12:33:11 +0200

    Delete run_baseline_algorithm_experiment.sh

--- run_baseline_algorithm_experiment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/tcsh
-#SBATCH --cpus-per-task=64
-#SBATCH --mem-per-cpu=8G
-#SBATCH --time=01:00:00
-source /cs/usr/netta.barak/.tcshrc
-conda activate evaluating_ginger
-python3 -u compare_ginger_to_baseline.py
