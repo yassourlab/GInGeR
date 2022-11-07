8739e2cef0e832751e4793f26dad413869b2d9c6
nettanetta <netta.barak@mail.huji.ac.il>
2022-11-07 16:49:21 +0200

    added writing to csvs+ added lots of tests files + added some (mostly) empty test python files + + renamed a python file

--- tests/ginger_test.py
+++ tests/ginger_test.py
@@ -1,17 +1,42 @@
 import unittest
 import ginger
+import pickle
+
+LAB = '/Volumes/netta.barak/lab'
+METADATA_PATH = f'{LAB}/Tools/UnifiedHumanGastrointestinalGenome/genomes-all_metadata.tsv'
 
 
 class GingerTest(unittest.TestCase):
-    def test_get_command_parser(self):
-        parser = ginger.get_command_parser()
-        parsed_args = vars(
-            parser.parse_args('short_reads_1 short_reads_2 genes_path output_folder --threads=32'.split(' ')))
-        self.assertEqual(parsed_args['short_reads_1'], 'short_reads_1')
-        self.assertEqual(parsed_args['short_reads_2'], 'short_reads_2')
-        self.assertEqual(parsed_args['genes_path'], 'genes_path')
-        self.assertEqual(parsed_args['output_folder'], 'output_folder')
-        self.assertEqual(parsed_args['threads'], 32)
+    def test_write_context_level_output_to_csv(self):
+        with open(f'{LAB}/Projects/hybrid_assembler/test_run/output_pickled.pkl', 'rb') as f:
+            output = pickle.load(f)
+        test_dict = {}
+        counter = 0
+        for gene_species_tuple, matches_list in output.items():
+            test_dict[gene_species_tuple] = matches_list
+            counter += 1
+            if counter > 20:
+                break
+        with open('context_level_output.pkl', 'wb') as f:
+            pickle.dump(test_dict, f)
+
+        ginger.write_context_level_output_to_csv(test_dict,
+                                                 f'{LAB}/Projects/hybrid_assembler/GInGeR/test_outputs/context_level_output_test.csv')
+
+    def test_aggregate_context_level_output_to_species_level_output_and_write_csv(self):
+        ginger.aggregate_context_level_output_to_species_level_output_and_write_csv(
+            'test_files/context_level_output_test.csv', METADATA_PATH,
+            f'{LAB}/Projects/hybrid_assembler/GInGeR/test_outputs/species_level_output_test.csv')
+
+    # def test_get_command_parser(self):
+    #     parser = ginger.get_command_parser()
+    #     parsed_args = vars(
+    #         parser.parse_args('short_reads_1 short_reads_2 genes_path output_folder --threads=32'.split(' ')))
+    #     self.assertEqual(parsed_args['short_reads_1'], 'short_reads_1')
+    #     self.assertEqual(parsed_args['short_reads_2'], 'short_reads_2')
+    #     self.assertEqual(parsed_args['genes_path'], 'genes_path')
+    #     self.assertEqual(parsed_args['output_folder'], 'output_folder')
+    #     self.assertEqual(parsed_args['threads'], 32)
 
 
 if __name__ == '__main__':
