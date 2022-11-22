365abe616f225bf0fdafcf6c1fc5fc66f104e5c3
Haimasree Bhattacharya <haimasree.il@gmail.com>
2022-11-22 16:44:55 +0200

    Adding full environment and sys spec on phoenix-gw

--- run_ginger_job.sh
+++ run_ginger_job.sh
@@ -1,7 +1,8 @@
-#!/bin/tcsh
-#SBATCH --cpus-per-task=16
-#SBATCH --mem-per-cpu=16G
+#!/bin/csh
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=8G
 #SBATCH --time=01:00:00
-## conda env create -f /sci/labs/morani/morani/icore-data/lab/Projects/hybrid_assembler/GInGeR/ginger_conda_env.yml
-conda activate evaluating_ginger
-python3 -u /sci/labs/morani/morani/icore-data/lab/Projects/hybrid_assembler/GInGeR/run_ginger_from_python.py
+#SBATCH --output=/cs/labs/morani/haimasree/GInGeR_test_run/O-%x.%j.out
+#SBATCH --error=/cs/labs/morani/haimasree/GInGeR_test_run/E-%x.%j.err
+conda activate  gingerminimal
+python3 -u /cs/labs/morani/haimasree/GInGeR/run_ginger_from_python.py
