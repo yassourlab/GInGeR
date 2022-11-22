c7648fccf55a7f42d394e90a31827afe4594ffa1
Haimasree Bhattacharya <haimasree.il@gmail.com>
2022-11-22 16:46:07 +0200

    Correcting the env name

--- run_ginger_job.sh
+++ run_ginger_job.sh
@@ -4,5 +4,5 @@
 #SBATCH --time=01:00:00
 #SBATCH --output=/cs/labs/morani/haimasree/GInGeR_test_run/O-%x.%j.out
 #SBATCH --error=/cs/labs/morani/haimasree/GInGeR_test_run/E-%x.%j.err
-conda activate  gingerminimal
+conda activate GInGeR-minimal
 python3 -u /cs/labs/morani/haimasree/GInGeR/run_ginger_from_python.py
