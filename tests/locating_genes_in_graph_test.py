4b4a10092d608597afeb001ce79e8a964e5cec67
nettabarak <netta.barak@mail.huji.ac.il>
2022-12-14 13:47:30 +0200

    changed expected files in context_level_matches.csv and species_level_matches.csv removed ginger_test.py adapted tests in locating_genes_in_graph_test and pipeline_utils_test.py

--- tests/locating_genes_in_graph_test.py
+++ tests/locating_genes_in_graph_test.py
@@ -1,10 +1,5 @@
 import unittest
-import locating_genes_in_graph as lg
-
-ASSEMBLY_DIR = 'test_files/assembly_dir'
-GENE_PIDENT_FILTERING_TH = 0.9
-GENES_PATH = 'test_files/genes.fasta'
-TEMP_FOLDER = 'temp_folder'
+from ginger import locating_genes_in_graph as lg
 
 
 class MyTestCase(unittest.TestCase):
@@ -12,21 +7,8 @@ class MyTestCase(unittest.TestCase):
     # find_genes_in_contigs is covered by tests in sequence_alignment_utils.py
     # locate_genes_in_graph is covered by testing get_genes_to_contigs_with_nodes_list which is the main function here
     def test_get_nodes_dict_from_fastg_file(self):
-        nodes_with_edges_and_sequences = lg.get_nodes_dict_from_fastg_file(ASSEMBLY_DIR + '/assembly_graph.fastg')
-        self.assertEqual(len(nodes_with_edges_and_sequences), 55897)
-
-    # def test_get_genes_to_contigs_with_nodes_list(self): # TODO you are here
-    #     # # Subject_10042055__Scaffold_1__Start_21742__End_22848	1107	0	1107	+	NODE_2634_length_3645_cov_4.273259	3645	850	1957	1094	1107	0	NM:i:13	ms:i:1042	AS:i:1042	nn:i:0	tp:A:S	cm:i:151	s1:i:962	de:f:0.0117	rl:i:0	cg:Z:1107M
-    #     genes_to_contigs = [
-    #         mc.GeneContigMatch(
-    #             mc.PafLine("Subject_10042055__Scaffold_1__Start_21742__End_22848", "1107", "0", "1107", "+",
-    #                        "NODE_2634_length_3645_cov_4", "3645", "850", "1957", "1094", "1107", "0", ""))
-    #     ]
-
-    #     genes_to_contigs
-    #     lg.get_genes_to_contigs_with_nodes_list(genes_to_contigs: Iterator[
-    #         mc.GeneContigMatch], assembly_graph: nx.DiGraph,
-    #     nodes_sequences_dict: Dict[str, SeqIO.SeqRecord], assembly_dir: str)
+        nodes_with_edges_and_sequences = lg.get_nodes_dict_from_fastg_file('test_files/SPAdes/assembly_graph.fastg')
+        self.assertEqual(len(nodes_with_edges_and_sequences), 2)
 
     if __name__ == '__main__':
         unittest.main()
--- tests/pipeline_utils_test.py
+++ tests/pipeline_utils_test.py
@@ -1,15 +1,31 @@
 import unittest
-import pipeline_utils as pu
+import os
+
+from ginger import pipeline_utils as pu
 
 
 class MyTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.context_level_output_path = 'test_files/context_level_matches.csv'
+        cls.metadata_path = '../ginger/UHGG-metadata.tsv'
+        cls.species_level_output_path = 'test_species_level_matches.csv'
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        os.remove(cls.species_level_output_path)
+
     def test_aggregate_context_level_output_to_species_level_output_and_write_csv(self):
-        context_level_output_path = '../e2e_test_output/context_level_matches.csv'
-        metadata_path = '../ginger/UHGG-metadata.tsv'
-        species_level_output_path = '../e2e_test_output/context_level_matches.csv'
-        pu.aggregate_context_level_output_to_species_level_output_and_write_csv(context_level_output_path,
-                                                                                metadata_path,
-                                                                                species_level_output_path, 10)
+        pu.aggregate_context_level_output_to_species_level_output_and_write_csv(self.context_level_output_path,
+                                                                                self.metadata_path,
+                                                                                self.species_level_output_path, 1)
+        self.assertTrue(os.path.exists(self.species_level_output_path))
+
+        with open(self.species_level_output_path, 'r') as f1, open(
+                'test_files/species_level_matches.csv', 'r') as f2:
+            lines_1 = f1.readlines()
+            lines_2 = f2.readlines()
+        self.assertListEqual(lines_1, lines_2)
 
 
 if __name__ == '__main__':
