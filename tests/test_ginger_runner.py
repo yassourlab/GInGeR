5183c631e2b0d6866620ef84b39c1b3bb6ef48a1
Haimasree <haimasree.il@gmail.com>
2022-12-15 12:34:54 +0200

    chekcing subset of tests

--- tests/test_ginger_runner.py
+++ tests/test_ginger_runner.py
@@ -6,11 +6,15 @@ from click.testing import CliRunner
 from ginger.ginger_runner import run_ginger_e2e
 import os
 
-SPADES_OUTPUT = 'test_files/SPAdes'
+from tests import helper
+
+TEST_FILES = helper.get_filedir()
+
+SPADES_OUTPUT = f'{TEST_FILES}/SPAdes'
 
 
 def run_meta_or_hybrid_spades_mock(short_reads_1, short_reads_2, long_reads, output_folder, threads):
-    copy_tree('test_files/SPAdes', output_folder)
+    copy_tree(f'{TEST_FILES}/SPAdes', output_folder)
     return output_folder
 
 
@@ -23,26 +27,26 @@ class GingerRunnerTest(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         rmtree(cls.ginger_out_dir)
-        os.remove('test_files/merged_filtered_ref_db.mmi')
+        os.remove(f'{TEST_FILES}/merged_filtered_ref_db.mmi')
 
     @patch('ginger.assembly_utils.run_meta_or_hybrid_spades', run_meta_or_hybrid_spades_mock)
     def test_ginger_e2e_skip_kraken(self):
         runner = CliRunner()
         result = runner.invoke(run_ginger_e2e,
-                               f'test_files/ecoli_1K_1.fq.gz test_files/ecoli_1K_2.fq.gz test_files/test_gene.fasta {self.ginger_out_dir} --merged-filtered-fasta test_files/merged_filtered_ref_db.fasta.gz --metadata-path ../ginger/UHGG-metadata.tsv --max-species-representatives 1'.split(
+                               f'{TEST_FILES}/ecoli_1K_1.fq.gz {TEST_FILES}/ecoli_1K_2.fq.gz {TEST_FILES}/test_gene.fasta {self.ginger_out_dir} --merged-filtered-fasta {TEST_FILES}/merged_filtered_ref_db.fasta.gz --metadata-path ginger/UHGG-metadata.tsv --max-species-representatives 1'.split(
                                    ' '))
         self.assertEqual(result.exit_code, 0, str(result.exception))
         self.assertTrue(os.path.exists(f'{self.ginger_out_dir}/context_level_matches.csv'))
         self.assertTrue(os.path.exists(f'{self.ginger_out_dir}/species_level_matches.csv'))
 
         with open(f'{self.ginger_out_dir}/context_level_matches.csv', 'r') as f1, open(
-                'test_files/context_level_matches.csv', 'r') as f2:
+                f'{TEST_FILES}/context_level_matches.csv', 'r') as f2:
             lines_1 = f1.readlines()
             lines_2 = f2.readlines()
         self.assertListEqual(lines_1, lines_2)
 
         with open(f'{self.ginger_out_dir}/species_level_matches.csv', 'r') as f1, open(
-                'test_files/species_level_matches.csv', 'r') as f2:
+                f'{TEST_FILES}/species_level_matches.csv', 'r') as f2:
             lines_1 = f1.readlines()
             lines_2 = f2.readlines()
         self.assertListEqual(lines_1, lines_2)
@@ -50,3 +54,4 @@ class GingerRunnerTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
