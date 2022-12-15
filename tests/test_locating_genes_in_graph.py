0d25f60f69ae5a6ad18431abb2b9370dd6af93a4
nettanetta <netta.barak@mail.huji.ac.il>
2022-12-15 12:44:30 +0200

    trying to see if using  Path(__file__).resolve().parent can fix the problem with CI

--- tests/test_locating_genes_in_graph.py
+++ tests/test_locating_genes_in_graph.py
@@ -1,5 +1,6 @@
 import unittest
 from ginger import locating_genes_in_graph as lg
+from pathlib import Path
 
 
 class LocatingGenesInGraphTest(unittest.TestCase):
@@ -7,8 +8,11 @@ class LocatingGenesInGraphTest(unittest.TestCase):
     # find_genes_in_contigs is covered by tests in sequence_alignment_utils.py
     # locate_genes_in_graph is covered by testing get_genes_to_contigs_with_nodes_list which is the main function here
     def test_get_nodes_dict_from_fastg_file(self):
-        nodes_with_edges_and_sequences = lg.get_nodes_dict_from_fastg_file('data/SPAdes/assembly_graph.fastg')
+        print(str(Path(__file__).resolve().parent))
+        nodes_with_edges_and_sequences = lg.get_nodes_dict_from_fastg_file(
+            str(Path(__file__).resolve().parent) + '/data/SPAdes/assembly_graph.fastg')
         self.assertEqual(len(nodes_with_edges_and_sequences), 2)
+
 
 if __name__ == '__main__':
     unittest.main()
