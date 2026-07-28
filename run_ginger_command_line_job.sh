de45e9b6655a215128f9c3b9c221ad6872c71a90
nettanetta <netta.barak@mail.huji.ac.il>
2026-07-28 14:28:49 +0300

    removed some files that were not used and should not be public

--- run_ginger_command_line_job.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/tcsh
-#SBATCH --cpus-per-task=16
-#SBATCH --mem-per-cpu=16G
-#SBATCH --time=01:00:00
-conda activate evaluating_ginger
-python3 -u ginger.py tests/test_files/ecoli_1K_1.fq.gz tests/test_files/ecoli_1K_2.fq.gz tests/test_files/test_gene.fasta -t 16 --species-inclusion-threshold 0.1
